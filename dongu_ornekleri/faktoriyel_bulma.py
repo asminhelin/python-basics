6d94a1314701b5cb0d105e57e3fbdbccf85ddd4e
asminhelin <asminhelink1306@gmail.com>
2025-08-17 18:29:48 +0300

    Add factorial example using loops

---
+++ dongu_ornekleri/faktoriyel_bulma.py
@@ -0,0 +1,19 @@
+print("""**************************
+Faktöriyel Bulma Programı
+      
+Çıkmak için 'Q'ya basın.
+**************************
+""")
+
+while True:
+    sayı=input("Sayı:")
+    if(sayı=="q"):
+        print("Program sonlandırılıyor.")
+        break
+    else:
+        sayı=int(sayı)
+        faktoriyel=1
+
+        for i in range(2,sayı+1):
+            faktoriyel*=i
+        print("Faktöriyel:",faktoriyel)
