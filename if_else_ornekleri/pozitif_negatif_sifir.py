b14a03d10b612486d25f3b372bf6a66676a09155
Asmin Helin Kahraman <asminhelink1306@gmail.com>
2026-06-29 17:27:51 +0300

    add number classification

---
+++ if_else_ornekleri/pozitif_negatif_sifir.py
@@ -0,0 +1,9 @@
+sayı=float(input("Bir sayı girin: "))
+
+if sayı>=0:
+    if sayı==0:
+        print("Sayı 0'a eşit")
+    else:
+        print("Sayı Pozitiftir")
+else:
+    print("Sayı Negatiftir")
--- if_else_ornekleri/sayi_negatif_mi.py
+++
@@ -1,5 +0,0 @@
-sayı=int(input("Sayı: "))
-if sayı<0:
-    print("Negatif Sayı")
-else:
-    print("Pozitif Sayı")
