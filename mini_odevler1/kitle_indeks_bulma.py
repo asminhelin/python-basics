1b2356b6c406854719c109a9783b6b5d637ae8e3
Asmin Helin Kahraman <asminhelink1306@gmail.com>
2026-06-30 18:49:53 +0300

    add bmi calculator

--- mini_odevler1/kitle_indeks_bulma.py
+++ mini_odevler1/kitle_indeks_bulma.py
@@ -2,5 +2,15 @@ print("Vücut Kitle İndeksi Hesaplama")
 
 boy=float(input("Boy:"))
 kilo=float(input("Kilo:"))
+vki=float(kilo/(boy**2))
 
-print("Vücut Kitle İndeksi:",kilo/boy**2)
+print("Vücut Kitle İndeksi:",vki)
+
+if (vki<18.5):
+    print("Zayıf")
+elif (vki>=18.5 and vki<25):
+    print("Normal")
+elif (vki>=25 and vki<30):
+    print("Fazla Kilolu")
+else:
+    print("Obez")
