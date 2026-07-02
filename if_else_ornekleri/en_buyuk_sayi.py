ea9b1f3c74ec24a34922ec2ec5a574d702109ab7
Asmin Helin Kahraman <asminhelink1306@gmail.com>
2026-07-02 19:14:45 +0300

    add largest number finder

---
+++ if_else_ornekleri/en_buyuk_sayi.py
@@ -0,0 +1,10 @@
+a=int(input("Birinci sayı:"))
+b=int(input("İkinci sayı:"))
+c=int(input("Üçüncü sayı:"))
+
+if (a>=b and a>=c):
+    print("En büyük sayı:",a)
+elif (b>=a and b>=c):
+    print("En büyük sayı:",b)
+elif (c>=a and c>=b):
+    print("En büyük sayı:",c)
