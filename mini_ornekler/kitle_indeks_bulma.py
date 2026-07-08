c4e6763650e373b67531594576e7a8b5ab6c1597
Asmin Helin Kahraman <asminhelink1306@gmail.com>
2026-07-08 12:57:25 +0300

    Rename folder

---
+++ mini_ornekler/kitle_indeks_bulma.py
@@ -0,0 +1,16 @@
+print("Vücut Kitle İndeksi Hesaplama")
+
+boy=float(input("Boy:"))
+kilo=float(input("Kilo:"))
+vki=float(kilo/(boy**2))
+
+print("Vücut Kitle İndeksi:",vki)
+
+if (vki<18.5):
+    print("Zayıf")
+elif (vki>=18.5 and vki<25):
+    print("Normal")
+elif (vki>=25 and vki<30):
+    print("Fazla Kilolu")
+else:
+    print("Obez")
