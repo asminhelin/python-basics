3c997b80b5b52625baa3e775ded7452e9c8e3cbd
Asmin Helin Kahraman <asminhelink1306@gmail.com>
2026-07-08 13:19:57 +0300

    Add fuel cost calculation

--- mini_ornekler/yakit_hesaplama.py
+++ mini_ornekler/yakit_hesaplama.py
@@ -1,3 +1,4 @@
 yakan_miktar=float(input("Km'de ne kadar yakar:"))
 km=float(input("Kaç km yol yapıldı:"))
-print("Ödenecek tutar {} tl".format(yakan_miktar*km))
+
+print("Ödenecek Tutar {} TL".format(yakan_miktar*km))
