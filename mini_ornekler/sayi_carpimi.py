085c3fc548dd523619ea599fdb74efd1c4c15535
Asmin Helin Kahraman <asminhelink1306@gmail.com>
2026-07-08 13:16:12 +0300

    Add three-number multiplication

--- mini_ornekler/sayi_carpimi.py
+++ mini_ornekler/sayi_carpimi.py
@@ -2,4 +2,4 @@ a=int(input("1. Sayıyı gir: "))
 b=int(input("2. Sayıyı gir: "))
 c=int(input("3. Sayıyı gir: "))
 çarpım= a*b*c
-print("{} x {} x {} = {}'dir.".format(a,b,c,çarpım))
+print("{} x {} x {} = {}'dir".format(a,b,c,çarpım))
