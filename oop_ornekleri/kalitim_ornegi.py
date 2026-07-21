0f5a7fd4b615596270d8f829f3a3c9216606376c
Asmin Helin Kahraman <asminhelink1306@gmail.com>
2026-07-21 14:41:58 +0300

    Add inheritance and method overriding example

--- oop_ornekleri/kalitim_ornegi.py
+++ oop_ornekleri/kalitim_ornegi.py
@@ -1,6 +1,5 @@
 class Çalışan():
     def __init__(self,isim,maaş,departman):
-        print("Çalışan sınıfının init fonksiyonu")
         self.isim=isim
         self.maaş=maaş
         self.departman=departman
@@ -15,13 +14,33 @@ Departman:{}""".format(self.isim,self.maaş,self.departman))
     def departman_değiştir(self,yeni_departman):
         self.departman=yeni_departman
 
-class Yönetici(Çalışan):
-    pass
+    def zam_yap(self,zam_miktarı):
+        self.maaş+=zam_miktarı
 
-yönetici1=Yönetici("Ayşe Aslan",95000,"Bilişim")
+class Yönetici(Çalışan):
+    def __init__(self,isim,maaş,departman,sorumlu_kişi_sayısı):
+        super().__init__(isim,maaş,departman)
+        self.sorumlu_kişi_sayısı=sorumlu_kişi_sayısı
+
+    def bilgileri_göster(self):
+        print("""
+Yönetici Sınıfının Bilgileri
+İsim:{}
+Maaş:{}
+Departman:{}
+Sorumlu Kişi Sayısı:{}""".format(self.isim,self.maaş,self.departman,self.sorumlu_kişi_sayısı))
+
+çalışan1=Çalışan("Ahmet Güçlü",70000,"Bilişim")
+yönetici1=Yönetici("Ayşe Aslan",95000,"Bilişim",10)
+
+çalışan1.bilgileri_göster()
 yönetici1.bilgileri_göster()
 
 print()
 
 yönetici1.departman_değiştir("İnsan Kaynakları")
+çalışan1.zam_yap(5000)
+
+çalışan1.bilgileri_göster()
 yönetici1.bilgileri_göster()
+
