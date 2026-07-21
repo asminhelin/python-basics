cb5de539312cdf3b4c682fc2e4fb8d51cae7398b
Asmin Helin Kahraman <asminhelink1306@gmail.com>
2026-07-21 14:57:16 +0300

    Improve root calculation

--- mini_ornekler/kok_bulma.py
+++ mini_ornekler/kok_bulma.py
@@ -1,10 +1,16 @@
-a = int(input("a="))
-b = int(input("b="))
-c = int(input("c="))
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
 
-delta = b**2-(4*a*c)
+if (a==0):
+    print("Bu ikinci dereceden bir denklem değildir.")
+else:
+    delta = b**2-(4*a*c)
 
-x1 = (-b-delta**0.5)/(2*a)
-x2 = (-b+delta**0.5)/(2*a)
-
-print("Birinci Kök= {}\nİkinci Kök= {}\n".format(x1,x2))
+    if (delta < 0):
+        print("Denklemin gerçek kökü yoktur.")
+    else:
+        x1 = (-b-delta**0.5)/(2*a)
+        x2 = (-b+delta**0.5)/(2*a)
+        
+        print("Birinci Kök= {}\nİkinci Kök= {}\n".format(x1,x2))
